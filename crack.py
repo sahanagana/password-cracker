81dc9fb7f911e56e80f76543a9f11d3aef013fde
sahanagana <70218712+sahanagana@users.noreply.github.com>
2022-02-08 19:48:37 -0600

    add dictionary attack functionality

--- crack.py
+++ crack.py
@@ -1,28 +1,45 @@
 import argparse
+import hashlib
 #run with 'python3 crack.py' + extension
 #arguments
 parser = argparse.ArgumentParser(description='python password cracker')
-parser.add_argument('-m', 'mode', help = 'b for bruteforce or d for dictionary attack', required= False)
+parser.add_argument('-m', '--mode', help = 'b for bruteforce or d for dictionary attack', required= False)
 parser.add_argument('-i', '--input', help = 'Input password hash', required= False)
 parser.add_argument('-h', '--hash', help = 'hash type, md5, sha256, or pt (for plaintext)', required= False)
 args = parser.parse_args()
 #dictionary attack--------------------------------
-if (args.mode=='d'):
+if args.mode=='d':
     print('starting dictionary attack...')
     #take in file from user and open
     filename = input('Enter dictionary file name:')
     with open(filename, 'r') as dictfile:
         #loop through it line by line
         for line in dictfile:
-            #hash the line
-            #compare result to password
-
+            #run da function to compare
+            if dictionary_attack(line, args.input)==True:
+                print('Matched!')
+                break
+            else:
+                continue
+        #otherwise
+        print('Couldn\'t find match rip lol')
 
 #brute force attack-------------------------------
-elif (args.mode=='b'):
+elif args.mode=='b':
     print('starting brute force attack...')
+    with open('passwords.txt','r') as passwords:
+        print(passwords)
 
 #default------------------------------------------
-else
+else:
     print('bruh')
     quit()
+
+#functions----------------------------------------
+def dictionary_attack(word, target):
+    wordbytes= word.encode('utf-8')
+    if(args.hash!= 'p'):
+        wordhash = args.hash(wordbytes)
+        digest = wordhash.hexdigest()
+    if digest == target:
+        return True
